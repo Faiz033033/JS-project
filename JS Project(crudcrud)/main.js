9260e37032c934cd58c4cc2a70bb92a391d82438
faiz033033 <feju302@gmail.com>
2023-04-17 23:20:18 +0530

    Get method implement

--- JS Project(crudcrud)/main.js
+++ JS Project(crudcrud)/main.js
@@ -1,25 +1,51 @@
+// Save appointment to server
 function saveAppointment() {
-    const name = document.getElementById("name").value;
-    const age = document.getElementById("age").value;
-    const email = document.getElementById("email").value;
-  
-    // Create an object 
-    const appointment = {
-      name,
-      age,
-      email
-    };
-  
-    // Send a POST 
-    axios.post('https://crudcrud.com/api/9c834735a96a4af8bb9ea52cd9f7034b/appointment', appointment)
-      .then(response => {
-        // Alert
-        alert("Your appointment has been saved!");
-      })
-      .catch(error => {
-        // Alert 
-        alert("There was an error saving your appointment. Please try again later.");
-        console.error(error);
+  const name = document.getElementById("name").value;
+  const age = document.getElementById("age").value;
+  const email = document.getElementById("email").value;
+
+  // Create an object
+  const appointment = {
+    name,
+    age,
+    email
+  };
+
+  // Send POST request to server
+  axios.post("https://crudcrud.com/api/122902386e2043738db116110312a864/appointmentdetail", appointment)
+    .then(response => {
+      console.log(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+// Get all appointments from server
+function getAppointment() {
+  axios.get("https://crudcrud.com/api/122902386e2043738db116110312a864/appointmentdetail")
+    .then(response => {
+      console.log(response.data);
+      const appointments = response.data;
+      const table = document.createElement('table');
+      table.innerHTML = `
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Email</th>
+        </tr>
+      `;
+      appointments.forEach(appointment => {
+        const row = table.insertRow();
+        row.innerHTML = `
+          <td>${appointment.name}</td>
+          <td>${appointment.age}</td>
+          <td>${appointment.email}</td>
+        `;
       });
-  }
-  
+      document.body.appendChild(table);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
